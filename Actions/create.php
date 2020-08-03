641f2acae6a994c746d6900668f3312465de25ee
Victor <augustovictormarinho@gmail.com>
2020-08-03 18:33:58 -0300

    Update

--- Actions/create.php
+++ Actions/create.php
@@ -9,16 +9,6 @@
 	$novaRegiao = $_POST['regiao'];
 	$novaUf = $_POST['uf'];
 
-	$query = $pdo->prepare("SELECT ibge, nome, uf FROM cidades");
-	$query->execute();
-	$resultado = $query->fetchAll(PDO::FETCH_ASSOC);
-
-	print_r($resultado);
-
-	if (condition) {
-		
-	}
-
 	$query = $pdo->prepare("INSERT INTO cidades(`ibge`, `uf`, `cidade`, `latitude`, `longitude`, `regiao`) VALUES(:ibge , :uf, :cidade, :latitude, :longitude, :regiao)");
 
 	$query->bindValue(":ibge", $novaIbge);
@@ -28,8 +18,8 @@
 	$query->bindValue(":regiao", $novaRegiao);
 	$query->bindValue(":uf", $novaUf);
 
-	//$query->execute();
+	$query->execute();
 
-	//header('Location: ../index.php');
+	header('Location: ../index.php');
 	
 ?>
